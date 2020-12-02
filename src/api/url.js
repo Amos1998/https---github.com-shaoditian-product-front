d2a6021207120ccc570700f1115c6b52135f16f5
Amos1998 <43166786+Amos1998@users.noreply.github.com>
2020-12-02 16:29:52 +0800

    Revert "cusPro"
    This reverts commit b504446502494dd82d2ad34d57ab61105fdb5ec1.

--- src/api/url.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by zyp on 2019/7/11.
- */
-import axios from 'axios'
-
-export const zhuBaseURL = 'https://shop.ie0.com/webapi/'
-// export const baseURL = 'http://47.242.130.50:9999/'
-export const baseURL = 'http://192.168.0.47:9999/'
